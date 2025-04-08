3d058745e47b733a93a978127da3b59518ffb70c
Ashish Pandey <pandeyashish0507@gmail.com>
2025-04-08 20:53:15 +0530

    personal changes

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const dbUri = 'your-mongo-db-url'
+const dbUri = 'mongodb+srv://pandeyashish0507:1234@cluster0.xmwle.mongodb.net/book_register_db?retryWrites=true&w=majority&appName=Cluster0'
 
 
 module.exports = () => mongoose.connect(dbUri)
